4ad007ff64a7a3aaf6c415f0cd03cd91f9967f57
kochoru <kochoru333@gmail.com>
2017-11-09 17:25:08 +0900

    create service class

--- src/main/java/com/example/typing/config/SecurityConfig.java
+++ src/main/java/com/example/typing/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.typing.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
---
+++ src/main/java/com/example/typing/controller/PlayerController.java
@@ -0,0 +1,23 @@
+package com.example.typing.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.typing.dto.Player;
+import com.example.typing.service.PlayerService;
+
+@RestController
+public class PlayerController {
+
+    @Autowired
+    PlayerService playerService;
+
+    // TODO: idとipアドレスだけでよい？
+    @PostMapping()
+    public Player confirmUser(Model model) {
+        model.addAttribute(new Player());
+
+    }
+}
