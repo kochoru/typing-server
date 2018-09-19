b8aec52541b2c1209290856be1ea7fa29c662c9d
kochoru <kochoru333@gmail.com>
2018-09-19 21:29:22 +0900

    update library version

--- src/main/java/com/example/typing/config/AppConfig.java
+++ src/main/java/com/example/typing/config/AppConfig.java
@@ -1,18 +1,9 @@
 package com.example.typing.config;
 
-import javax.sql.DataSource;
-
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,40 +12,7 @@ import com.fasterxml.jackson.databind.util.StdDateFormat;
 @Configuration
 @ComponentScan(basePackages = "com.example.typing")
 @EnableTransactionManagement
-@MapperScan(basePackages = "com.example.typing.mapper")
 public class AppConfig {
-
-    private String url;
-
-    private String driverClassName;
-
-    private String username;
-
-    private String password;
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create()
-                .driverClassName(this.driverClassName)
-                .url(this.url)
-                .password(this.password)
-                .username(this.username)
-                .build();
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(this.dataSource());
-    }
-
-    @Bean
-    public SqlSessionFactoryBean sqlSessionFactory() {
-        SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
-        sessionFactoryBean.setDataSource(dataSource());
-        sessionFactoryBean.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
-        return sessionFactoryBean;
-    }
 
     @Bean
     public ObjectMapper objectMapper() {
